0773ba100ee34ba8801b1dd88ee1a0fd30cf8716
Guido Schöpping <gusco@schopping.net>
2019-10-08 08:20:24 +0200

    Report cucumber

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "features", glue = "stepDefinitions")
+@CucumberOptions(features = "features", format = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"}, 
+                 glue = "stepDefinitions")
 
 public class TestRunner {
 }
