f3af947da176b38ef056baf0ef1559a6e3db9772
Guido Schöpping <gusco@schopping.net>
2019-11-06 20:50:16 +0100

    Read YAML

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,29 +1,89 @@
 package main;
 
+import EXIFFile.ReadAddress;
 import EXIFFile.ReadYaml;
+import org.yaml.snakeyaml.TypeDescription;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 
 public class main {
-    static String address = "Eiffeltower";
 
-    public static void main(String[] args) {
-        ReadYaml readYaml = null;
-        try {
-            readYaml = new ReadYaml("Z:\\workspace\\resources\\start_plain.yml");
-            System.out.println(readYaml.getTimeLine(0));
-//            Iterable<Object> objectIterable = readYaml.getObjectAllYaml();
-//            for (Object o : objectIterable) {
-//                System.out.println("element type: " + o.getClass());
-//                System.out.println(o);
-//            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    public class Customer {
+        private String firstName;
+//        private String lastName;
+//        private int age;
+//        private List<Contact> contactDetails;
+//        private Address homeAddress;
+        // getters and setters
+
+
+        public String getFirstName() {
+            return firstName;
         }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+//        public String getLastName() {
+//            return lastName;
+//        }
+//
+//        public void setLastName(String lastName) {
+//            this.lastName = lastName;
+//        }
+//
+//        public int getAge() {
+//            return age;
+//        }
+//
+//        public void setAge(int age) {
+//            this.age = age;
+//        }
+//
+//        public List<Contact> getContactDetails() {
+//            return contactDetails;
+//        }
+//
+//        public void setContactDetails(List<Contact> contactDetails) {
+//            this.contactDetails = contactDetails;
+//        }
+//
+//        public Address getHomeAddress() {
+//            return homeAddress;
+//        }
+//
+//        public void setHomeAddress(Address homeAddress) {
+//            this.homeAddress = homeAddress;
+//        }
+
     }
 
+    public static void main(String[] args) throws IOException {
+//        Yaml yaml = new Yaml();
+//        String document = "\n- Hesperiidae\n- Papilionidae\n- Apatelodidae\n- Epiplemidae";
+//        List<String> list = (List<String>) yaml.load(document);
+//        System.out.println(list.get(0));
+
+        String document = "firstName: \"John\"\n" +
+                "lastName: \"Doe\"\n" +
+                "age: 31\n";
+
+
+        Constructor constructor = new Constructor(Customer.class);
+//        TypeDescription customerDescription = new TypeDescription(Customer.class);
+        Yaml yaml = new Yaml();
+        InputStream input = Files.newInputStream(Path.of("Z:\\workspace\\resources\\address.yaml"));
+        Map customer = yaml.load(input);
+        System.out.println(customer.get("homeAddress"));
+    }
 
 }
