33f2cd70479640b13fe481b76c78727d38ee93c6
Guido Schöpping <gusco@schopping.net>
2019-11-20 21:21:31 +0100

    Use arguments in main

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -10,25 +10,28 @@ public class main {
 
     public static void main(String[] args) {
 
-        String configFile = "X:\\Onbewerkt\\start.yml";
-        String startdir = "X:\\Onbewerkt";
-        String log4jConfigFile = "C:\\Program Files\\apache-maven-3.6.2\\bin\\log4j.properties";
+        if (args.length > 2) {
+            String configFile = args[0];
+            String startdir = args[1];
+            String log4jConfigFile = args[2];
 
-        PropertyConfigurator.configure(log4jConfigFile);
-        log.info(String.format("Configuration file:\t%s", configFile));
-        log.info(String.format("Start dicrectory:\t%s", startdir));
+            PropertyConfigurator.configure(log4jConfigFile);
+            log.info(String.format("Configuration file:\t%s", configFile));
+            log.info(String.format("Start dicrectory:\t%s", startdir));
 
 
-        try {
-            RenameFiles renameFiles = new RenameFiles(log, startdir, configFile);
-            renameFiles.RenameRootFiles();
-            renameFiles.RenameTimelapsFiles(true);
-            renameFiles.RenameTimelapsFiles(false);
-        }
-        catch (Exception e) {
-            log.error(e);
+            try {
+                RenameFiles renameFiles = new RenameFiles(log, startdir, configFile);
+                renameFiles.RenameRootFiles();
+                renameFiles.RenameTimelapsFiles(true);
+                renameFiles.RenameTimelapsFiles(false);
+            } catch (Exception e) {
+                log.error(e);
 //            System.out.println(e.getMessage());
+            }
         }
+        else
+            log.warn("Missing arguments");
 
     }
 
